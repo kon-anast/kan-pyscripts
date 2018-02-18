6427675ad2c121c41d7496e587d1696629a2c329
kon-anast <kon.anastasakis@gmail.com>
2018-02-18 23:17:52 +0200

    Updates

--- crypto/crypto_01.py
+++ crypto/crypto_01.py
@@ -21,13 +21,10 @@ in_coins_prices = open(
 coins_prices = pd.read_csv(
     in_coins_prices, sep='\t', encoding='utf-8')
 
-#Summation of coins from diferent exchanges
+#Summation of same coins from diferent exchanges
 mycoins = mycoins.groupby('Symbol')['Balance'].sum()
 
 # Connection of data
-#results = pd.merge(
-    #mycoins, coins_prices, on='Symbol')
-
 results = coins_prices.merge(
     mycoins.to_frame(), left_on='Symbol', right_index=True)
 
@@ -45,37 +42,47 @@ results['Market Cap'] = results['Market Cap'].str.replace(',', '')
 results['Circulating Supply'] = results['Circulating Supply'].str.replace('$', '')
 results['Circulating Supply'] = results['Circulating Supply'].str.replace('?', '0')
 results['Circulating Supply'] = results['Circulating Supply'].str.replace(',', '')
-
 results['Price'] = results['Price'].astype(float)
 results['Balance'] = results['Balance'].astype(float)
 
 #Calculating Total
 results['Total'] = results['Balance'] * results['Price']
-
+print(results)
 results.columns = pd.Series(results.columns).str.replace('Name_y', 'Name')
+
 columns = [
    'Name', 'Symbol', 'Price', 'Balance', 'Total', 'Market Cap']
 
+results = results[columns]
+
+#Remuve coins
+print('!!!ALERT THESE ARE COINS WITH SAME SYMBOLS!!!')
+duplicated = results[results.duplicated(['Symbol'], keep=False)]
+print(duplicated)
+
+print('These coins are remuved: KingN Coin, BlockCAT, BatCoin, Catcoin')
+results = results[results.Name != 'KingN Coin']
+results = results[results.Name != 'BlockCAT']
+results = results[results.Name != 'BatCoin']
+results = results[results.Name != 'Catcoin']
+
+duplicated = results[results.duplicated(['Symbol'], keep=False)]
+print(duplicated)
+print('Test!: if Index: [] is emty it is OK, \n')
+
+#Print on screen
+print(results)
+print('\n')
 
 #Count totals
 SumUSD = results['Total'].sum()
 
-#Print on screen
-results = results[columns]
-print(results)
-print('\n')
-
-print('!!!ALERT THESE ARE COINS WITH SAME SYMBOLS!!!')
-ids = results[results.duplicated(['Symbol'], keep=False)]
-print(ids)
-
-print('\n')
 print('Sum in $ = ', SumUSD, 'without removing the fees (0.1)') # noqa
 print('Sum in $ = ', SumUSD * 0.9, 'net and conversion fees removed (*0.9)')
 SumEU = SumUSD * 0.8
 print('Sum in € = ', SumEU, '*0.8 (USD to EURO)')
 print('Sum in € = ', SumEU * 0.9, 'net and conversion fees removed (*0.9)')
-print('Sum in € = ', SumEU * 0.9 - 3100, 'profit (-3100 first investment)')
+print('Sum in € = ', SumEU * 0.9 - 3100, 'profit (-310 first investment)')
 
 
 # Save data
